131c928d99695d17eaffddbbe9c593fd5cedecc4
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-29 04:16:26 +0530

    home slice made and store.js made and we connect the store with app.js

---
+++ src/store/homeSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const homeSlice = createSlice({
+  name: "home",
+  initialState: {
+    url: {},
+    genres: {},
+  },
+  reducers: {
+    getApiConfiguration: (state, action) => {
+      state.url = action.payload;
+    },
+    getGenres: (state, action) => {
+      state.genres = action.payload;
+    },
+  },
+});
+
+//action creators are generated for each case reducer function
+
+export const { getApiConfiguration, getGenres } = homeSlice.actions;
+
+export default homeSlice.reducer;
