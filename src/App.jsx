a0165ca863d79e11a61abe4514486a82b841182f
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-29 02:54:38 +0530

    api is been fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
+import { fetchDataFromApi } from "./utils/api";
 function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
   return <div className="App">App</div>;
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOCKEN          ;
 
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
 };
 
-export const fetchDataFromApi = async (url, params) => {
+export const fetchDataFromApi = async (url, params) => { // url is like end point which will be different for everything (movie ,trending, popular etc etc)
   try {
     const { data } = await axios.get(BASE_URL + url, {
       headers,
