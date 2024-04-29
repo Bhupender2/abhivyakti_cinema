3edbf3613ce1d15901a52dcfacfcdfc6549d49d6
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-29 18:27:18 +0530

    data is stored from the api to the redux store using actions and then we used that stored data from the redux store using useSelector

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 import { fetchDataFromApi } from "./utils/api";
 function App() {
+  const dispactch = useDispatch();
+
+  // to use the data from the redux store we use useSelector
+  const { url } = useSelector((state) => state.home); // useSelector basically returns the home(homeSlice) object from the store
+
+  console.log(url)
   useEffect(() => {
     apiTesting();
   }, []);
@@ -9,9 +17,12 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispactch(getApiConfiguration(res));
     });
   };
-  return <div className="App">App</div>;
+  return <div className="App">APP{url?.total_pages}</div>;
 }
 
 export default App;
+
+// with the help of usedispatch and use selector we can store the data coming from an API into a store and with the help of actions , we can  dispactch the actions and store the data in the global reux store
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOCKEN          ;
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOCKEN;
 
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
 };
 
-export const fetchDataFromApi = async (url, params) => { // url is like end point which will be different for everything (movie ,trending, popular etc etc)
+export const fetchDataFromApi = async (url, params) => {
+  // url is like end point which will be different for everything (movie ,trending, popular etc etc)
   try {
     const { data } = await axios.get(BASE_URL + url, {
       headers,
