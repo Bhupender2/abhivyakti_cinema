f695b75f3f42715496dc8a2d8458ffbcaf93777e
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-29 22:46:53 +0530

    getting the full path of backdrop image using useSelector in herobanner.js and adding that returned url path into bg path so that we can have the full image path

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./style.scss";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux"; // to show the data store in redux store we use useSelector hook
 import useFetch from "../../../hooks/useFetch";
 
 const HeroBanner = () => {
@@ -8,10 +9,14 @@ const HeroBanner = () => {
   const [background, setBackground] = useState("");
   const [query, setQuery] = useState("");
   const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home); //we will get homeSlice object
   const { data, loading } = useFetch("/movie/upcoming");
   useEffect(() => {
-    const bg = data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
-    setBackground(bg)
+    // by adding url.backdrop we are getting the full image http path that we want
+    const bg =
+      url.backdrop + 
+      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+    setBackground(bg);
   }, [data]); // we'll use optional chaining taaki hmaari application break na ho jab tak optional chaining h aage ka code execute nahi hoga agar vo undefined hoga wahi se value return karlega
 
   const searchQueryHandler = (event) => {
@@ -22,6 +27,9 @@ const HeroBanner = () => {
   };
   return (
     <div className="heroBanner">
+    <div className="backdrop-img">
+      
+    </div>
       <div className="wrapper">
         <div className="heroBannerContent">
           <span className="title">Welcome.</span>
