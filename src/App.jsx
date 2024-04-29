a0c15890dff66db3e9d6700c0790d9667578ef4d
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-29 19:55:41 +0530

    heroBanner component is made and same state is set

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { useDispatch, useSelector } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
 
 import { fetchDataFromApi } from "./utils/api";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
 function App() {
   const dispactch = useDispatch();
 
   // to use the data from the redux store we use useSelector
   const { url } = useSelector((state) => state.home); // useSelector basically returns the home(homeSlice) object from the store
 
-  console.log(url)
+  console.log(url);
   useEffect(() => {
     apiTesting();
   }, []);
@@ -20,7 +31,20 @@ function App() {
       dispactch(getApiConfiguration(res));
     });
   };
-  return <div className="App">APP{url?.total_pages}</div>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} /> ?
+        {/* media type is like is it a movie or a tv show*/}
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import "./style.scss"
+import HeroBanner from './heroBanner/HeroBanner';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+    <HeroBanner/>
+    </div>
   )
 }
 
