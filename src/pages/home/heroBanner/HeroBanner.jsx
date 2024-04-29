e92543cda9ad7480d69c746077cba563ceeedebc
Bhupender2 <b.mehtaa2021@gmail.com>
2024-04-29 20:05:11 +0530

    code is formatted

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,39 +1,40 @@
-import React , {useState}from 'react';
+import React, { useState } from "react";
 import "./style.scss";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const HeroBanner = () => {
   // on refreshing we call the api and set the background image on every refresh so we need a state(state mein jaake save hojaati h )
-  const [background, setBackground]=useState("")
-  const [query, setQuery]=useState("")
-  const navigate=useNavigate();
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
-  const searchQueryHandler=(event)=>{
- if(event.key==="Enter" && query.length>0){ // only on enter we have to run the search query not like ki every key pe searchQuery run karahe h 
-    navigate(`/search/${query}`) // searchResults page pe jaana h apne ko
- }
-  }
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      // only on enter we have to run the search query not like ki every key pe searchQuery run karahe h
+      navigate(`/search/${query}`); // searchResults page pe jaana h apne ko
+    }
+  };
   return (
-  <div className='heroBanner'>
-    <div className='wrapper'>
-      <div className='heroBannerContent'>
-        <span className='title'>Welcome.</span>
-        <span className='subTitle'>
-           Millions of movies , Tv shows and people to discover. Explore now
-        </span>
-        <div className='searchInput'>
-          <input
-          type="text"
-           placeholder='Search for a movie or tv show...'
-           onChange={(e)=>setQuery(e.target.value)}
-           onKeyUp={searchQueryHandler}
-           />
-           <button>Search</button>
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies , Tv shows and people to discover. Explore now
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tv show..."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
