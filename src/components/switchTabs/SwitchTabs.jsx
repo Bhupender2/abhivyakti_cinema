457771358694a68a2317d79c714ca4f13e0e7b8e
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-01 01:17:34 +0530

    trending and switchTabs component made

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SwitchTabs = () => {
+  return (
+    <div>SwitchTabs</div>
+  )
+}
+
+export default SwitchTabs
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import "./style.scss"
-import HeroBanner from './heroBanner/HeroBanner';
+import React from "react";
+import "./style.scss";
+import HeroBanner from "./heroBanner/HeroBanner";
+import Trending from "./trending/Trending";
 
 const Home = () => {
   return (
-    <div className='homePage'>
-    <HeroBanner/>
-  <div style={{height:"1000px"}}></div>
+    <div className="homePage">
+      <HeroBanner />
+      <Trending />
+      <div style={{ height: "1000px" }}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
