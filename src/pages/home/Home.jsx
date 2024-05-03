d557837a619181fd6adc3c8e4677ad708df5bab9
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-04 03:00:32 +0530

    Top rated component also completd

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,14 +3,15 @@ import "./style.scss";
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
 import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
       <Trending />
-      <Popular/>  
-      <div style={{ height: "1000px" }}></div>
+      <Popular />
+      <TopRated/>
     </div>
   );
 };
