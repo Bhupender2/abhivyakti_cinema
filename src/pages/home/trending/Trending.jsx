d32c26292199aa77dec327674a5519175ee1bfe4
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-03 13:42:09 +0530

    switch tab and carousel section completed but its not completed i think i have written some classname name wrong

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Carousel from "../../../components/carousel/Carousel";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
@@ -16,8 +17,12 @@ const Trending = () => {
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
         {/*static nahi hoga dynamic hoga yeh jaise yeh h "month* daalenge toh vo bhi aana chahiye WE WILL MAKE THIS REUSABLE and iska control (onChange) vo iske parent component k pass rahega*/}
       </ContentWrapper>
+
+      <Carousel  data={data?.results} loading={loading}/> 
     </div>
   );
 };
 
 export default Trending;
+
+// whenever you are sending the api data (properties)as props always use optional chaning  
