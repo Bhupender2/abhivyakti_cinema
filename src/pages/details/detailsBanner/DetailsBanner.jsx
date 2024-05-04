40950a5286550389d5480889af655d39be8bf699
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-05 02:55:21 +0530

    writer added to

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -102,21 +102,32 @@ const DetailsBanner = ({ video, crew }) => {
                         </div>
                       )}
                     </div>
-                   {director?.length>0 &&(
-                     <div className="info">
-                       <span className="text bold">
-                        Director:{" "}
-                       </span>
-                       <span className="text">
-                        {director?.map((d,i)=>(
-                          <span key={i}>
-                            {d.name}
-                            {director.length-1 !==i && ", "}
-                          </span>
-                        ))}
-                       </span>
-                     </div>
-                   )}
+                    {director?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Director: </span>
+                        <span className="text">
+                          {director?.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {director.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+                    {writer?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Writer: </span>
+                        <span className="text">
+                          {writer?.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {writer.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
                   </div>
                 </div>
               </ContentWrapper>
