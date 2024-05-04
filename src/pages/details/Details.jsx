eb36aa2c26e078b68284fb2483c1d89f9428fc32
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-05 01:29:06 +0530

    credits and videos api created using useFetch() hook

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -5,12 +5,15 @@ import { useParams } from "react-router-dom";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 const Details = () => {
-  // const { mediaType, id } = useParams(); // we gave the params name mediaType and Id in app.js react router
-  // const { data, loading } = useFetch(`/${mediaType}/${id}`);
+  const { mediaType, id } = useParams(); // we gave the params name mediaType and Id in app.js react router
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`); // all the videos related to that particular movie or tv shows will be returned from here
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  ); // dusri api call karwani padegi coz same api k andar credits actor ka data nahi h (endpoint change kark hum ye cheez achieve karenge)
 
   return (
     <div>
-      <DetailsBanner />
+      <DetailsBanner video={data.results[0]} />
     </div>
   );
 };
