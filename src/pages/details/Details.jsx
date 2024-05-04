290b5f6edad4c60fe3fa863bd9076b984408bf77
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-05 02:53:39 +0530

    all the directors name shown done using api useFtech() costum hook by adding endpoins in it

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -13,7 +13,7 @@ const Details = () => {
 
   return (
     <div>
-      <DetailsBanner video={data.results[0]} />
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew}/> {/*home details banner mein bass ek pehli video hi pass karni h trailor ki*/}
     </div>
   );
 };
--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -19,6 +19,10 @@ const DetailsBanner = ({ video, crew }) => {
 
   const { url } = useSelector((state) => state.home); //data extract karliya hmare store mein se using useSelector
   const _genres = data?.genres?.map((g) => g.id); // we need id to do beacuse the way we created our genre component is passing id well get the genres like this {id:{}, id:{},id:{}}
+  const director = crew?.filter((f) => f.job === "Director");
+  const writer = crew?.filter(
+    (f) => f.job === "ScreenPlay" || f.job === "Story" || f.job === "Writer"
+  );
 
   const toHoursAndMinutes = (totalMinutes) => {
     const hours = Math.floor(totalMinutes / 60);
@@ -98,6 +102,21 @@ const DetailsBanner = ({ video, crew }) => {
                         </div>
                       )}
                     </div>
+                   {director?.length>0 &&(
+                     <div className="info">
+                       <span className="text bold">
+                        Director:{" "}
+                       </span>
+                       <span className="text">
+                        {director?.map((d,i)=>(
+                          <span key={i}>
+                            {d.name}
+                            {director.length-1 !==i && ", "}
+                          </span>
+                        ))}
+                       </span>
+                     </div>
+                   )}
                   </div>
                 </div>
               </ContentWrapper>
