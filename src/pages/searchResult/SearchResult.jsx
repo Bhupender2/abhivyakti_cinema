dc7465a0e0aa13177955f0ed89f10bfe29b92774
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-06 05:42:02 +0530

    spinner component made

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import "./style.scss"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"; // before ? if we pass any dynamic value in a path like /:id it is a params;
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import "./style.scss";
+import { fetchDataFromApi } from "../../utils/api"; //not using costum hook we are using original one's .
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import noResults from "../../assets/no-results.png";
 
 const SearchResult = () => {
-  return (
-    <div>SearchResult</div>
-  )
-}
+  const [data, setData] = useState(null); //the search data
 
-export default SearchResult
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams(); // hook to store the params from URL
+
+  const fetchInitialData = () => {
+    setLoading(true); // data has not arrived yet so here we set it true
+    fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        setData(res);
+        setPageNum((prev) => prev + 1);
+        setLoading(false); // data has arrived so its false now
+      }
+    );
+  };
+
+  useEffect(() => {
+    fetchInitialData(); // it will be executed after the component is rendered
+  }, [query]);
+
+  return <div className="searchResultsPage">SearchResult</div>;
+};
+
+export default SearchResult;
